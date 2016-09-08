ba67fc3d45fe9daaaee29a4ca17a40e5690e1eef
OksanaPiskunova <piskunova.oksana2014@yandex.ru>
2016-09-08 21:35:46 +0300

    Added initial realization

---
+++ Tracer/Tracer/ConsoleTraceResultFormatter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracer
+{
+    public class ConsoleTraceResultFormatter : ITraceResultFormatter
+    {
+        void Format(TraceResult traceResult)
+        {
+
+        }
+    }
+}
---
+++ Tracer/Tracer/Tracer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracer
+{
+    public class Tracer : ITracer
+    {
+        private static volatile Tracer instance = null;
+        private static readonly object syncRoot = new object();
+
+        private Tracer()
+        {
+
+        }
+
+        void StartTrace()
+        {
+            lock (syncRoot)
+            {
+
+            }
+        }
+
+        void StopTrace()
+        {
+            lock (syncRoot)
+            {
+
+            }
+        }
+
+        TraceResult GetTraceResult()
+        {
+            return null;
+        }
+
+        public static Tracer Instance()
+        {
+            if (instance == null)
+            {
+                lock (syncRoot)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Tracer();
+                    }
+                }                    
+            }               
+            return instance;
+        }
+    }
+}
