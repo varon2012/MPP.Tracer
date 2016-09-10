8dc73240af31a908002b395d6753eb2ee5690230
Mikhail Snitavets <mksn13@gmail.com>
2016-09-10 17:29:13 +0300

    General entities created.

---
+++ Tracer/Tracer/Tracer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracer
+{
+    class Tracer: ITracer
+    {
+
+        private ConcurrentStack<TracerNode> _tray = new ConcurrentStack<TracerNode>();
+
+        public void StartTrace()
+        {
+            
+        }
+
+        public void StopTrace()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TraceResult GetTraceResult()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ Tracer/Tracer/TracerNode.cs
@@ -0,0 +1,7 @@
+﻿namespace Tracer
+{
+    internal class TracerNode
+    {
+        
+    }
+}
