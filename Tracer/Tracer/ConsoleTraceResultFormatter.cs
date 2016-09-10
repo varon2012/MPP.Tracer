fd8aa61465ccbb8a617a5e21d2fb3cb184cff697
OksanaPiskunova <piskunova.oksana2014@yandex.ru>
2016-09-10 17:28:26 +0300

    Added console output

--- Tracer/Tracer/ConsoleTraceResultFormatter.cs
+++ Tracer/Tracer/ConsoleTraceResultFormatter.cs
@@ -1,16 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Tracer
 {
     public class ConsoleTraceResultFormatter : ITraceResultFormatter
     {
+        private string baseIndent = "\t";
         public void Format(TraceResult traceResult)
         {
+            Dictionary<int, TraceResultThreadNode> threadNodes = traceResult.ThreadNodes;
+            foreach (var threadId in threadNodes.Keys)
+            {
+                Console.WriteLine("Thread ID: {0}", threadId);
 
+                if (threadNodes[threadId].MethodNodesList != null)
+                {
+                    foreach (var node in threadNodes[threadId].MethodNodesList)
+                    {
+                        PrintMethodNode(node, baseIndent);
+                        Console.WriteLine();
+                    }
+                }
+
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+
+        private void PrintMethodNode(TraceResultMethodNode methodNode, string indent)
+        {
+            Console.WriteLine("{0}Method: {1}", indent, methodNode.MethodName);
+            Console.WriteLine("{0}Class: {1}", indent, methodNode.ClassName);
+            Console.WriteLine("{0}Params: {1}", indent, methodNode.ParamCount);
+            Console.WriteLine("{0}Time (ms): {1}", indent, methodNode.TotalTime.TotalMilliseconds);
+
+            if (methodNode.InsertedNodes != null)
+            {
+                string newIndent = indent + baseIndent;
+                foreach (var node in methodNode.InsertedNodes)
+                {
+                    PrintMethodNode(node, newIndent);
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
