76216443173da0b03451eb2de60404faf1c6d029
Mikhail Snitovets <mksn13@gmail.com>
2016-09-12 00:29:38 +0300

    Simple test added

---
+++ Test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Tracer;
+
+namespace Test
+{
+    class Program
+    {
+
+        static Tracer.Tracer _tracer = Tracer.Tracer.Instance;
+
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(() =>
+            {
+                _tracer.StartTrace();
+                Thread.Sleep(69);
+                _tracer.StopTrace();
+            });
+        }
+    }
+}
