73485441f8e0fb3657b84735ac03fedcf2e07b13
JonesTwink <arthur.zhukovski@gmail.com>
2016-09-11 21:46:47 +0300

    Add testing application (Tester) to the Tracer solution

---
+++ Tracer/Tester/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tracer;
+namespace Tester
+{
+    class Program
+    {
+        public static ITest test;
+
+        static void Main(string[] args)
+        {
+            Init();
+        }
+        private static void Init()
+        {
+            test = new SingleThreadTest();
+            test.Run();
+        }
+    }
+}
---
+++ Tracer/Tester/SingleThreadTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tracer;
+namespace Tester
+{
+    class SingleThreadTest : ITest
+    {
+        private ITracer tracer = new Tracer.Tracer();
+        public void Run()
+        {
+            RunCycle();
+
+            tracer.StopTrace();
+            TraceResult result = tracer.GetTraceResult();
+
+            Console.WriteLine("Время: " + 0.001*result.time + " сек.");
+        }
+
+        private void RunCycle()
+        {
+            tracer.StartTrace();
+            for (int i = 0; i < 204800000; i++)
+            {
+                int a = 1;
+                a += i;
+            }
+        }
+
+    }
+}
