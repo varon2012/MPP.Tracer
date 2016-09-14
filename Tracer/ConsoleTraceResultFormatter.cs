44b86d11054f0161b28ffe48ee8570b794939720
Ivan <vana.shimko@gmail.com>
2016-09-14 20:18:28 +0300

    Add empty formatters
    - interface ITraceResultFormatter
    - empty classes for console output and xml formatting

---
+++ Tracer/ConsoleTraceResultFormatter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracer
+{
+    class ConsoleTraceResultFormatter : ITraceResultFormatter
+    {
+        public void Format(TraceResult traceResult)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
