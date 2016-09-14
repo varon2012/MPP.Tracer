e9cffe6263542f5fac6dd82149b6274125a0b90b
Ivan <vana.shimko@gmail.com>
2016-09-14 10:09:11 +0300

    Add singleton Tracer
    - Tracer implements ITracer (defined in the task)

--- Tracer/Tracer.cs
+++ Tracer/Tracer.cs
@@ -6,7 +6,51 @@ using System.Threading.Tasks;
 
 namespace Tracer
 {
-    public class Tracer
+    public class Tracer : ITracer
     {
+        private TraceResult fTraceResult;
+
+        private static volatile Tracer _tracerInstance;
+        private static readonly object SyncRoot = new object();
+
+        public void StartTrace()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopTrace()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TraceResult GeTraceResult()
+        {
+            throw new NotImplementedException();
+        }
+
+        // Static
+
+        public static Tracer GetInstance()
+        {
+            if (_tracerInstance == null)
+            {
+                lock (SyncRoot)
+                {
+                    if (_tracerInstance == null)
+                    {
+                        _tracerInstance = new Tracer();
+                    }
+                }
+            }
+
+            return _tracerInstance;
+        }
+
+        // Internal
+
+        private Tracer()
+        {
+            fTraceResult = new TraceResult();
+        }
     }
 }
