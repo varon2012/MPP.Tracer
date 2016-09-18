9f73c3b5a2527be8dfed818bbb8711697c265076
Andrey Tatarenko <andrey17076@gmail.com>
2016-09-18 21:17:47 +0300

    Add ITrace interface and implement it

--- Tracer/Tracer.cs
+++ Tracer/Tracer.cs
@@ -2,9 +2,28 @@
 
 namespace Tracer
 {
-    public class Tracer
+    public class Tracer : ITracer
     {
+        private static readonly Tracer instance = new Tracer();
 
+        private Tracer(){}
+
+        public static Tracer Instance => instance;
+
+        public void StartTrace()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopTrace()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TraceResult GetTraceResult()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
 
