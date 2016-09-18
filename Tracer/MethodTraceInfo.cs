578d68628d122f58223b5e4647ccae529c1e48a8
Andrey Tatarenko <andrey17076@gmail.com>
2016-09-18 21:15:59 +0300

    Add MethodTraceInfo enxity

---
+++ Tracer/MethodTraceInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Tracer
+{
+    public class MethodTraceInfo
+    {
+        private List<MethodTraceInfo> nestedMethodsTraceInfo;
+
+        public MethodTraceInfo()
+        {
+            nestedMethodsTraceInfo = new List<MethodTraceInfo>();
+        }
+
+        public string Name { get; set; }
+        public string ClassName { get; set; }
+        public long Duration { get; set; }
+        public int ArgumentsCount { get; set; }
+    }
+}
