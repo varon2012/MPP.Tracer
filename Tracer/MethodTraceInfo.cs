43a238daf7a7bbf82f690a6bb2d6a5db8388a3db
Andrey Tatarenko <andrey17076@gmail.com>
2016-09-18 21:56:39 +0300

    Add MethodTraceInfo implementation

--- Tracer/MethodTraceInfo.cs
+++ Tracer/MethodTraceInfo.cs
@@ -1,19 +1,41 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace Tracer
 {
     public class MethodTraceInfo
     {
         private List<MethodTraceInfo> nestedMethodsTraceInfo;
+        private Stopwatch timer;
+        private MethodBase method;
 
-        public MethodTraceInfo()
+        public MethodTraceInfo(MethodBase method)
         {
+            this.method = method;
             nestedMethodsTraceInfo = new List<MethodTraceInfo>();
+            StartTrace();
         }
 
-        public string Name { get; set; }
-        public string ClassName { get; set; }
-        public long Duration { get; set; }
-        public int ArgumentsCount { get; set; }
+        public string Name => method.Name;
+        public string ClassName => method.DeclaringType.Name;
+        public long Duration => timer.ElapsedMilliseconds;
+        public int ArgumentsCount => method.GetParameters().Length;
+
+        public void StopTrace()
+        {
+            timer.Stop();
+        }
+
+        public void AddNestedMethod(MethodTraceInfo methodTraceInfo)
+        {
+            nestedMethodsTraceInfo.Add(methodTraceInfo);
+        }
+
+        private void StartTrace()
+        {
+            timer = new Stopwatch();
+            timer.Start();
+        }
     }
 }
