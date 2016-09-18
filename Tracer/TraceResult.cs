0091c4ee570caf8769eea06970cfa215c39f64c9
Andrey Tatarenko <andrey17076@gmail.com>
2016-09-18 21:16:56 +0300

    Add TraceResult entity

---
+++ Tracer/TraceResult.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Concurrent;
+
+namespace Tracer
+{
+    public class TraceResult
+    {
+        private ConcurrentDictionary<int, ThreadTraceInfo> threadsTraceInfo;
+
+        public TraceResult()
+        {
+            threadsTraceInfo = new ConcurrentDictionary<int, ThreadTraceInfo>();
+        }
+
+        public ConcurrentDictionary<int, ThreadTraceInfo> ThreadsTraceInfo => threadsTraceInfo;
+    }
+}
