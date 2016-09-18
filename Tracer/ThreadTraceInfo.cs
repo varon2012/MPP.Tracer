e5e3a2379f8d29aa9e8e47bde5c4f6cddccf46b3
Andrey Tatarenko <andrey17076@gmail.com>
2016-09-18 21:16:23 +0300

    Add Thread Trace Info entity

---
+++ Tracer/ThreadTraceInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Tracer
+{
+    public class ThreadTraceInfo
+    {
+        private List<MethodTraceInfo> methodsTraceInfo;
+
+        public ThreadTraceInfo()
+        {
+            methodsTraceInfo = new List<MethodTraceInfo>();
+        }
+
+        public long Duration { get; set; }
+        public List<MethodTraceInfo> MethodsTraceInfo => methodsTraceInfo;
+    }
+}
