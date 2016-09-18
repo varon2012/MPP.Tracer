858bdee340bcf1d08147cf3fb2c965b2f21cf9b3
Andrey Tatarenko <andrey17076@gmail.com>
2016-09-18 20:34:01 +0300

    Add solution sceleton

---
+++ Tracer/Tracer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Tracer
+{
+    public class Tracer
+    {
+
+    }
+}
+
+
---
+++ TracerTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TracerTest
+{
+    class Program
+    {
+      static void Main(string[] args)
+      {
+
+      }
+    }
+}
+
+
