ecc2392b679cee9b0528e7f5b43f9e95e6ca2587
Andrey Tatarenko <andrey17076@gmail.com>
2016-09-20 02:34:03 +0300

    Separate logic for theard tracing

---
+++ Tracer/Models/ThreadTraceResult.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Tracer.Models
+{
+    public class ThreadTraceResult
+    {
+        public ThreadTraceResult()
+        {
+            MethodsTraceResult = new List<MethodTraceResult>();
+        }
+
+        public List<MethodTraceResult> MethodsTraceResult { get; }
+        public long Duration { get; private set; }
+
+        public void AddMethodTraceResult(MethodTraceResult methodTraceResult)
+        {
+            MethodsTraceResult.Add(methodTraceResult);
+            Duration += methodTraceResult.Duration;
+        }
+    }
+}
---
+++ Tracer/Tracers/ThreadTracer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Tracer.Models;
+
+namespace Tracer.Tracers
+{
+    public class ThreadTracer
+    {
+        private readonly Stack<MethodTracer> _callStack;
+        private readonly ThreadTraceResult _threadTraceResult;
+
+        public ThreadTracer()
+        {
+            _threadTraceResult = new ThreadTraceResult();
+            _callStack = new Stack<MethodTracer>();
+            MethodTracers = new List<MethodTracer>();
+        }
+
+        public List<MethodTracer> MethodTracers { get; }
+
+        public void StartMethodTrace(MethodTracer methodTracer)
+        {
+            if (_callStack.Count == 0)
+                MethodTracers.Add(methodTracer);
+            else
+                _callStack.Peek().AddNestedMethodTracer(methodTracer);
+            _callStack.Push(methodTracer);
+        }
+
+        public void StopMethodTrace()
+        {
+            if (_callStack.Count <= 0) return;
+            MethodTracer methodTracer = _callStack.Pop();
+            methodTracer.StopTrace();
+            if (MethodTracers.Contains(methodTracer))
+                _threadTraceResult.AddMethodTraceResult(methodTracer.GetTraceResult());
+        }
+
+        public ThreadTraceResult GetTraceResult()
+        {
+            return _threadTraceResult;
+        }
+    }
+}
