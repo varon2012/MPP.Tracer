123d1136345fe9a78388007ef3d6375a9f973b7c
Dmitry <mr.blacky01@gmail.com>
2016-09-19 21:08:16 +0300

    + Add interface

---
+++ TracerProgram/Tracer/ITracer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracer
+{
+    interface ITracer
+    {
+        void StartTrace();
+
+        void StopTrace();
+
+        TraceResult GetTraceResult();
+    }
+}
