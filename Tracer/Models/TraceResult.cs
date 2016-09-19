6a2afce4101b3a7fbf11e1edb22773b16222825d
Andrey Tatarenko <andrey17076@gmail.com>
2016-09-20 02:36:11 +0300

    Separate logic for whole tracing

---
+++ Tracer/Models/TraceResult.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using Tracer.Models;
+using Tracer.Tracers;
+
+namespace Tracer
+{
+    public class TraceResult
+    {
+        public TraceResult(ConcurrentDictionary<int, ThreadTracer> threadTracers)
+        {
+            ThreadsTraceResult = new Dictionary<int, ThreadTraceResult>();
+            foreach (KeyValuePair<int, ThreadTracer> threadTracer in threadTracers)
+            {
+                ThreadsTraceResult.Add(threadTracer.Key, threadTracer.Value.GetTraceResult());
+            }
+        }
+
+        public Dictionary<int, ThreadTraceResult> ThreadsTraceResult { get; }
+    }
+}
---
+++ Tracer/Tracers/Tracer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading;
+using Tracer.Interfaces;
+using Tracer.Tracers;
+
+namespace Tracer
+{
+    public class Tracer : ITracer
+    {
+        private readonly ConcurrentDictionary<int, ThreadTracer> _threadTracers;
+
+        private Tracer()
+        {
+           _threadTracers = new ConcurrentDictionary<int, ThreadTracer>();
+        }
+
+        public static Tracer Instance { get; } = new Tracer();
+
+        public void StartTrace()
+        {
+            int currentThreadId = Thread.CurrentThread.ManagedThreadId;
+            MethodBase currentMethod = new StackTrace().GetFrame(1).GetMethod();
+            ThreadTracer threadTracer = _threadTracers.GetOrAdd(currentThreadId, new ThreadTracer());
+            threadTracer.StartMethodTrace(new MethodTracer(currentMethod));
+        }
+
+        public void StopTrace()
+        {
+            var currentThreadId = Thread.CurrentThread.ManagedThreadId;
+            _threadTracers[currentThreadId].StopMethodTrace();
+        }
+
+        public TraceResult GetTraceResult()
+        {
+            return new TraceResult(_threadTracers);
+        }
+    }
+}
+
+
