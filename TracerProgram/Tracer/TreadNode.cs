6ee8d9121091eb98755a887eb6de53a450e5107a
Dmitry <mr.blacky01@gmail.com>
2016-09-19 21:11:33 +0300

    + Add class that work with tracer find threads

---
+++ TracerProgram/Tracer/TreadNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracer
+{
+    public class ThreadNode
+    {
+        public int ID;
+        public List<MethodNode> Methods;
+
+        private int ValueOfWorkingMethods;
+        private readonly static object locker = new object();
+
+        public ThreadNode(int id)
+        {
+            ID = id;
+            Methods = new List<MethodNode>();
+            ValueOfWorkingMethods = 0;
+        }
+
+
+    }
+}
