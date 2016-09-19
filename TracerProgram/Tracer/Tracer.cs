d69eff799db22b585d19a8336aaf83ad22add8a9
Dmitry <mr.blacky01@gmail.com>
2016-09-19 21:13:37 +0300

    + Add base functionality of ITracer

--- TracerProgram/Tracer/Tracer.cs
+++ TracerProgram/Tracer/Tracer.cs
@@ -3,10 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace Tracer
 {
-    public class Class1
+    public class Tracer : ITracer
     {
+
+        private TraceResult resultsOfTrace;
+
+        public Tracer()
+        {
+            resultsOfTrace = new TraceResult();
+        }
+
+        public TraceResult GetTraceResult()
+        {
+
+            return resultsOfTrace;
+        }
+
+        public void StartTrace()
+        {
+            long time = GetCurrentTime();
+            MethodInfo curentMethod = GetMethodInfo();
+
+            
+        }
+
+        public void StopTrace()
+        {
+           
+        }
+
+        private long GetCurrentTime()
+        {
+            return DateTime.UtcNow.Ticks / TimeSpan.TicksPerMillisecond;
+        }
+
+        private MethodInfo GetMethodInfo()
+        {
+            StackTrace stackTrace = new StackTrace();
+            MethodBase methodBase = stackTrace.GetFrame(2).GetMethod();
+            string Name = methodBase.Name;
+            string Class = methodBase.DeclaringType.ToString();
+            int parametersNumber = methodBase.GetParameters().Length;
+
+            return new MethodInfo(Name,Class, parametersNumber);
+        }
     }
 }
