f669159cb44517dd307cde07b43fd63ce433b3ac
Andrey Tatarenko <andrey17076@gmail.com>
2016-09-20 00:31:53 +0300

    Separate logic from MethodTraceInfo

---
+++ Tracer/Models/MethodTraceResult.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace Tracer.Models
+{
+    public class MethodTraceResult : TraceResult
+    {
+        public MethodTraceResult(MethodBase methodBase)
+        {
+            Name = methodBase.Name;
+            ClassName = methodBase.DeclaringType?.Name;
+            ArgumentsCount = methodBase.GetParameters().Length;
+            NestedMethodsTraceInfo = new List<MethodTraceResult>();
+        }
+
+        public string Name { get; }
+        public string ClassName { get; }
+        public long Duration { get; set; }
+        public int ArgumentsCount { get; }
+        public List<MethodTraceResult> NestedMethodsTraceInfo { get; }
+
+        public void AddNestedMethodResult(MethodTraceResult methodTraceResult)
+        {
+            NestedMethodsTraceInfo.Add(methodTraceResult);
+        }
+
+    }
+}
---
+++ Tracer/Tracers/MethodTracer.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Reflection;
+using Tracer.Interfaces;
+using Tracer.Models;
+
+namespace Tracer.Tracers
+{
+    public class MethodTracer : ITracer
+    {
+
+        private readonly MethodTraceResult _methodTraceResult;
+        private Stopwatch _timer;
+
+        public MethodTracer(MethodBase methodBase)
+        {
+            _methodTraceResult = new MethodTraceResult(methodBase);
+            StartTrace();
+        }
+
+        public void StartTrace()
+        {
+            _timer = new Stopwatch();
+            _timer.Start();
+        }
+
+        public void StopTrace()
+        {
+            _timer.Stop();
+            _methodTraceResult.Duration = _timer.ElapsedMilliseconds;
+        }
+
+        public TraceResult GetTraceResult()
+        {
+            return _methodTraceResult;
+        }
+
+        public void AddNestedMethodResult(MethodTraceResult methodTraceResult)
+        {
+            _methodTraceResult.AddNestedMethodResult(methodTraceResult);
+        }
+    }
+}
