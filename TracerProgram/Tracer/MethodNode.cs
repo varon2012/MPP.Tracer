58b105b924fee84c477285275be9aa43da504f05
Dmitry <mr.blacky01@gmail.com>
2016-09-19 21:12:55 +0300

    + Add class that contain all methods of thread in trace work

---
+++ TracerProgram/Tracer/MethodNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracer
+{
+    public class MethodNode
+    {
+        private MethodInfo Info;
+        private int Heignt;
+
+        public MethodNode(MethodInfo info, int height)
+        {
+            Info = info;
+            Heignt = height;
+        }
+
+    }
+}
