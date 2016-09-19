31edb0f28c2276858eb16f5cd898a2a85a281932
Dmitry <mr.blacky01@gmail.com>
2016-09-20 01:04:35 +0300

    + Add methods AddMethod and StopMethod to work with tree structure in Tracer

--- TracerProgram/Tracer/TreadNode.cs
+++ TracerProgram/Tracer/TreadNode.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,19 +9,49 @@ namespace Tracer
 {
     public class ThreadNode
     {
-        public int ID;
-        public List<MethodNode> Methods;
-
+        public int ID { get; }
+        public List<MethodNode> MethodsTree;
         private int ValueOfWorkingMethods;
-        private readonly static object locker = new object();
 
         public ThreadNode(int id)
         {
             ID = id;
-            Methods = new List<MethodNode>();
+            MethodsTree = new List<MethodNode>();
             ValueOfWorkingMethods = 0;
         }
 
+        public void AddMethod(MethodInfo method)
+        {
+            if (MethodsTree != null)
+            {
+                ValueOfWorkingMethods++;
+                MethodsTree.Add(new MethodNode(method,ValueOfWorkingMethods));
+            }
+            else
+            {
+                throw new NullReferenceException("List of Methods is empty");
+            }         
+        }
 
+        public void StopMethod(long time)
+        {
+            if (MethodsTree != null)
+            {
+                bool fixTime = false;
+                int currentIndex = MethodsTree.Count() - 1;
+                while (fixTime == false)
+                {
+                    MethodNode temp = MethodsTree.ElementAt(currentIndex);
+                    if (temp.isWorking == true)
+                    {
+                        temp.Info.Time = time - temp.Info.Time;
+                        temp.isWorking = false;
+                        fixTime = true;
+                    }
+                    currentIndex--;
+                }
+                ValueOfWorkingMethods--;
+            }
+        }
     }
 }
