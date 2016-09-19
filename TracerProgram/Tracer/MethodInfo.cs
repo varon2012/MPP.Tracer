83f15aebd17fc4942ca3fb6a0d04de9e54eabd1e
Dmitry <mr.blacky01@gmail.com>
2016-09-19 21:17:40 +0300

    + Add description of class

--- TracerProgram/Tracer/MethodInfo.cs
+++ TracerProgram/Tracer/MethodInfo.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace Tracer
 {
-    class MethodInfo
+    public class MethodInfo
     {
+        public string Name { get; }
+        public string ClassName { get; }
+        public int ParamsNumber { get; }
+        private long Time;
+
+        public long TraceTime
+        {
+            get
+            {
+                return Time;
+            }
+            set
+            {
+                Time = (Time == -1) ? value : Time;
+            }
+        }
+
+        public MethodInfo(string name, string className, int paramsNumber,long Time)
+        {
+            Name = name;
+            ClassName = className;
+            ParamsNumber = paramsNumber;
+            Time = -1;
+        }
     }
 }
