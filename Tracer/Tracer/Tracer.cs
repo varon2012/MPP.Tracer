c3d86731e95b5c64cdc442f972134243de7b5fc1
JonesTwink <arthur.zhukovski@gmail.com>
2016-09-20 18:51:54 +0300

    [Fix] Improve incorrect time measurment in situations when a method is called multiple times within a single thread

--- Tracer/Tracer/Tracer.cs
+++ Tracer/Tracer/Tracer.cs
@@ -10,17 +10,14 @@ namespace Tracer
 {
     public class Tracer : ITracer
     {
-
         public static Tracer Instance { get { return lazy.Value; } }
         private static readonly Lazy<Tracer> lazy = new Lazy<Tracer>(() => new Tracer());
         private Tracer()
         {
         }
-
+        private Dictionary<int, Stack<TraceResultItem>> threadStack = new Dictionary<int, Stack<TraceResultItem>>();
         private TraceResult result { get; set; } = new TraceResult();
-        private StackTrace stackTrace { get; set; }
-        private KeyValuePair<int, StackFrame> stackFrame { get; set; }
-
+        private const int DistanceToCallerMethodInFrames = 2;
         private object threadLock = new object();
 
         public void StartTrace()
@@ -31,18 +28,48 @@ namespace Tracer
             }         
         }
 
+        private void AddTraceItem()
+        {
+            var stackTrace = new StackTrace();                     
+            TraceResultItem analyzedItem = GenerateTraceResultItem(stackTrace);
+            PushItemToThreadStack(analyzedItem);            
+            UpdateCallDepth(stackTrace.FrameCount);
+        }
+
+        private TraceResultItem GenerateTraceResultItem(StackTrace stackTrace)
+        {
+            StackFrame stackFrame = stackTrace.GetFrame(DistanceToCallerMethodInFrames);
+
+            var method = stackFrame.GetMethod();
+            var parametersAmount = method.GetParameters().Length;
+            var className = method.ReflectedType.Name;
+            var threadId = Thread.CurrentThread.ManagedThreadId;
+            var parentMethod = stackTrace.GetFrame(DistanceToCallerMethodInFrames+1).GetMethod().Name;
+
+            return new TraceResultItem(new Stopwatch(), className, method.Name, parametersAmount, threadId, stackTrace.FrameCount, parentMethod);
+        }
+
+        private void PushItemToThreadStack(TraceResultItem analyzedItem)
+        {
+            if (threadStack.ContainsKey(analyzedItem.threadId))
+            {
+                threadStack[analyzedItem.threadId].Push(analyzedItem);
+            }
+            else
+            {
+                threadStack.Add(analyzedItem.threadId, new Stack<TraceResultItem>());
+                threadStack[analyzedItem.threadId].Push(analyzedItem);
+                result.MarkThread(analyzedItem.threadId);
+            }
+        }
+
         public void StopTrace()
         {
             lock (threadLock)
             {
-                stackFrame = ResetStackFrame(2);
-
-                string currentMethodName = stackFrame.Value.GetMethod().Name;
-                int currentThreadId = Thread.CurrentThread.ManagedThreadId;
-                int currentDepth = stackFrame.Key;
-                TraceResultItem analyzedItem = result.Find(currentMethodName, currentThreadId, stackFrame.Key+1);
-
+                TraceResultItem analyzedItem = threadStack[Thread.CurrentThread.ManagedThreadId].Pop();
                 analyzedItem.StopRuntimeMeasuring();
+                result.Add(analyzedItem);
             }
         }
 
@@ -54,35 +81,6 @@ namespace Tracer
             }
         }
 
-        private void AddTraceItem()
-        {
-            stackFrame = ResetStackFrame(3);
-
-            var method = stackFrame.Value.GetMethod();
-            var parametersAmount = method.GetParameters().Length;
-            var className = method.ReflectedType.Name;
-            var threadId = Thread.CurrentThread.ManagedThreadId;
-            var parentMethod = GetParentMethodName();
-            var newAnalyzedItem = new TraceResultItem(new Stopwatch(), className, method.Name, parametersAmount, threadId, stackFrame.Key, parentMethod);
-            UpdateTraceResult(newAnalyzedItem, threadId);
-            UpdateCallDepth(stackFrame.Key);
-        }
-
-        private string GetParentMethodName()
-        {
-            return stackTrace.GetFrame(4).GetMethod().Name;
-        }
-
-        private KeyValuePair<int, StackFrame> ResetStackFrame(int depth)
-        {
-            stackTrace = new StackTrace();
-            return new KeyValuePair<int, StackFrame>(stackTrace.FrameCount, stackTrace.GetFrame(depth));           
-        }
-        private void UpdateTraceResult(TraceResultItem newAnalyzedItem, int threadId)
-        {
-            result.Add(newAnalyzedItem);
-            result.MarkThread(threadId);
-        }
         private void UpdateCallDepth(int newDepth)
         {
             if (newDepth > result.callDepth)
