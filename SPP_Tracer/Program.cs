b666b0f4ff0634e55ae31574f7d520da0d225d3d
Aleksander <blambermr@gmail.com>
2016-09-20 13:20:31 +0300

    fixed structure

--- SPP_Tracer/Program.cs
+++ SPP_Tracer/Program.cs
@@ -2,14 +2,61 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Tracer;
+
 
 namespace SPP_Tracer
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            var tracer = Tracer.Tracer.Instance;
+            tracer.StartTrace();
+            Inner1Method1();
+            Inner2Method1();
+            tracer.StopTrace();
+            new XmlFormatter().Format(tracer.TraceResult);
+        }
+
+        static void Inner1Method1()
+        {
+            var tracer = Tracer.Tracer.Instance;
+            tracer.StartTrace();
+            Inner1Method2();
+            tracer.StopTrace();
+        }
+
+        static void Inner1Method2()
+        {
+            var tracer = Tracer.Tracer.Instance;
+            tracer.StartTrace();
+            Thread.Sleep(100);
+            tracer.StopTrace();
+        }
+
+        static void Inner2Method1()
+        {
+            var tracer = Tracer.Tracer.Instance;
+
+            tracer.StartTrace();
+
+            Inner2Method2();
+
+            tracer.StopTrace();
+        }
+
+        static void Inner2Method2()
+        {
+            var tracer = Tracer.Tracer.Instance;
+
+            tracer.StartTrace();
+
+            Thread.Sleep(100);
+
+            tracer.StopTrace();
         }
     }
 }
