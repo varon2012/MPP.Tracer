716d769bd21b1a937f8b7ac3c913eafe5bf26d00
Andrey Tatarenko <andrey17076@gmail.com>
2016-09-20 03:06:39 +0300

    Improve test

--- TracerTest/Program.cs
+++ TracerTest/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Threading;
 using Tracer;
 using Tracer.Fromatters;
@@ -9,11 +10,37 @@ namespace TracerTest
     internal class Program
     {
         private static readonly ITracer Tracer = global::Tracer.Tracer.Instance;
+        private static readonly List<Thread> Threads = new List<Thread>();
 
-        private static void TestA()
+        private static void Main(string[] args)
         {
             Tracer.StartTrace();
-            Thread.Sleep(100);
+            StartMethodCallFlow();
+            StopMethodCallFlow();
+            Tracer.StopTrace();
+            PrintResult(Tracer.GetTraceResult());
+        }
+
+        private static void StartMethodCallFlow()
+        {
+            Threads.Add(new Thread(TestA));
+            Threads.Add(new Thread(TestB));
+            Threads.Add(new Thread(TestC));
+            Threads.ForEach((thread) => thread.Start());
+        }
+
+        private static void StopMethodCallFlow()
+        {
+            Threads.ForEach((thead) => thead.Join());
+        }
+
+        private static void TestA(object recursionLevelObj)
+        {
+            Tracer.StartTrace();
+            Thread.Sleep(10);
+            int recursionLevel = (int) (recursionLevelObj ?? 0);
+            if (recursionLevel < 5)
+                TestA(recursionLevel + 1);
             TestB();
             Tracer.StopTrace();
         }
@@ -21,23 +48,30 @@ namespace TracerTest
         private static void TestB()
         {
             Tracer.StartTrace();
-            Thread.Sleep(100);
+            Thread.Sleep(10);
             Tracer.StopTrace();
         }
 
-        private static void TestC(int a, int b)
+        private static void TestC()
         {
             Tracer.StartTrace();
             Thread.Sleep(100);
+            TestD(null, null);
             Tracer.StopTrace();
         }
 
-        private static void Main(string[] args)
+        private static void TestD(object a, object b)
         {
-            new Thread(TestA).Start();
-            TestB();
-            TestC(0, 1);
-            PrintResult(Tracer.GetTraceResult());
+            Tracer.StartTrace();
+            var threads = new List<Thread>();
+            for (int i = 0; i < 5; i++)
+            {
+                var thread = new Thread(TestB);
+                threads.Add(thread);
+                thread.Start();
+            }
+            threads.ForEach((thread) => thread.Join());
+            Tracer.StopTrace();
         }
 
         private static void PrintResult(TraceResult result)
