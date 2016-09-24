9808204b080b58a9b9dae71ca2b855454b22ea83
Andrey Tatarenko <andrey17076@gmail.com>
2016-09-24 23:41:54 +0300

    Add serialization formater and remake other to suit models

---
+++ Tracer/Formatters/ConsoleTraceResultFormatter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Tracer.Interfaces;
+using Tracer.Models;
+
+namespace Tracer.Formatters
+{
+    public class ConsoleTraceResultFormatter : ITraceResultFormatter
+    {
+        public void Format(TraceResult traceResult)
+        {           
+            foreach (ThreadTraceResult threadTraceResult in traceResult.ThreadsTraceResult)
+            {
+                Console.WriteLine(threadTraceResult);
+                PrintMethodsTraceResult(threadTraceResult.MethodsTraceResult, 0);
+            }
+        }
+
+        private static void PrintMethodsTraceResult(List<MethodTraceResult> methodsTraceResult, int nestingLevel)
+        {
+            methodsTraceResult.ForEach((traceResult) => PrintMethodTraceResult(traceResult, nestingLevel + 1));
+        }
+
+        private static void PrintMethodTraceResult(MethodTraceResult methodTraceResult, int nestingLevel)
+        {
+            Console.Write(GetIndent(nestingLevel));
+            Console.WriteLine(methodTraceResult);
+            PrintMethodsTraceResult(methodTraceResult.NestedMethodsTraceResult, nestingLevel);
+        }
+
+        private static string GetIndent(int nestingLevel)
+        {
+            return new string(' ', nestingLevel * 4);
+        }
+    }
+}
---
+++ Tracer/Formatters/XmlTraceResultFormatter.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.Xml.Linq;
+using Tracer.Interfaces;
+using Tracer.Models;
+
+namespace Tracer.Formatters
+{
+    public class XmlTraceResultFormatter : ITraceResultFormatter
+    {
+        private readonly Stream _outStream;
+
+        public XmlTraceResultFormatter(Stream outStream)
+        {
+            _outStream = outStream;
+        }
+
+        public void Format(TraceResult traceResult)
+        {
+            using (_outStream)
+            {
+                var document = new XDocument();
+                document.Add(GetRootElement(traceResult));
+                document.Save(_outStream);
+            }
+        }
+
+        private static XElement GetRootElement(TraceResult traceResult)
+        {
+            var rootElement = new XElement("root");
+            traceResult.ThreadsTraceResult.ForEach((thread) => rootElement.Add(GetThreadElement(thread)));    
+            return rootElement;
+        }
+
+        private static XElement GetThreadElement(ThreadTraceResult threadTraceResult)
+        {
+            var threadElement = new XElement("thread");
+            threadElement.Add(new XAttribute("id", threadTraceResult.Id));
+            threadElement.Add(new XAttribute("time", threadTraceResult.Duration));
+            threadTraceResult.MethodsTraceResult.ForEach((method) => threadElement.Add(GetMethodElement(method)));
+            return threadElement;
+        }
+
+        private static XElement GetMethodElement(MethodTraceResult methodTraceResult)
+        {
+            var methodElement = new XElement("method");
+            methodElement.Add(new XAttribute("name", methodTraceResult.Name));
+            methodElement.Add(new XAttribute("time", methodTraceResult.Duration));
+            methodElement.Add(new XAttribute("class", methodTraceResult.ClassName));
+            methodElement.Add(new XAttribute("params", methodTraceResult.ArgumentsCount));
+            methodTraceResult.NestedMethodsTraceResult.ForEach((nested) => methodElement.Add(GetMethodElement(nested)));
+            return methodElement;
+        }
+    }
+}
