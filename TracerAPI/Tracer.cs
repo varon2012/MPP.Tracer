e9e88186e53dc8da1684ed06d48238310704f030
lebedeanastasija <lebedeanastasija@yandex.by>
2016-09-26 16:10:45 +0300

    test commit

--- TracerAPI/Tracer.cs
+++ TracerAPI/Tracer.cs
@@ -8,11 +8,37 @@ using System.Diagnostics;
 
 namespace TracerAPI
 {
-    public static class Tracer
+    public class Tracer
     {
-        public static TraceResult TraceInfo = new TraceResult();
+        private TraceResult TraceInfo;
+        private static volatile Tracer instance = null;
+        private static readonly Object syncObj = new Object();
+       
 
-        public static void StartTrace()
+        private Tracer()
+        {
+            TraceInfo = new TraceResult();
+        }
+
+        public static Tracer Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    lock (syncObj)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Tracer();
+                        }                        
+                    }
+                }
+                return instance;
+            }
+        }
+
+        public void StartTrace()
         {
             DateTime startTime = DateTime.Now;
             StackTrace stackTrace = new StackTrace();
@@ -22,12 +48,12 @@ namespace TracerAPI
             int threadId = thread.ManagedThreadId; 
             string methodName = methodFrame.GetMethod().ToString();
             string parentName = parentFrame.GetMethod().ToString();
-            string methodClassName = "System Main";
+            string methodClassName = methodFrame.GetMethod().ReflectedType.ToString();
 
             Tracer.AddStartInfo(threadId, parentName, methodName, methodClassName, startTime);
   
         }
-        public static void StopTrace()
+        public void StopTrace()
         {
             DateTime stopTime = DateTime.Now;
 
@@ -45,20 +71,24 @@ namespace TracerAPI
 
         }
 
-        public static TraceResult GetTraceResult() { return TraceInfo; }
+        public TraceResult GetTraceResult() 
+        {
+            return TraceInfo; 
+        }
 
         public static void AddStartInfo(int threadId, string parentName,
-                                        string methodName, string methodClassName, DateTime startTime)
+                                        string methodName, string methodClassName, 
+                                        DateTime startTime)
         {
             Tree tree;           
-            tree = TraceInfo.GetTreeByThreadId(threadId);           
+            tree = Tracer.Instance.GetTraceResult().GetTreeByThreadId(threadId);           
             tree.AddNode(parentName, methodName, methodClassName, startTime);
         }
 
         public static void AddStopInfo(int threadId, string methodName, DateTime stopTime)
         {
             Tree tree;
-            tree = TraceInfo.GetTreeByThreadId(threadId);
+            tree = Tracer.Instance.GetTraceResult().GetTreeByThreadId(threadId);
             tree.CompleteNode(methodName, stopTime);
         }
     }
